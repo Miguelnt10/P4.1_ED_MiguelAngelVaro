3c9e7e40289620b44febdd6b71466c06b59f44be
Miguel Varo <migvarray@g.educaand.com>
2024-02-25 19:25:46 +0100

    Subo el fichero CalculadoraGit.java con dos metodos

--- CalculadoraGit/src/calculadoragit/CalculadoraGit.java
+++ CalculadoraGit/src/calculadoragit/CalculadoraGit.java
@@ -21,9 +21,14 @@ public class CalculadoraGit {
         int num2 = sc.nextInt();
 
         sumar(num1, num2);
+        restar(num1,num2);
     }
 
     private static void sumar(int num1, int num2) {
         System.out.println("La suma de los dos numeros es: " + (num1 + num2));
+    }
+
+    private static void restar(int num1, int num2) {
+        System.out.println("La resta de los dos numeros es: " + (num1 - num2));
     }
 }
